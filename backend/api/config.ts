7c1fb1ea2d224a992f959d9a46b73767b216f1d5
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-03 11:11:46 +0100

    Update package.json scripts and dependencies

---
+++ backend/api/config.ts
@@ -0,0 +1,3 @@
+export const getAppName = () => {
+	return 'Vite-React-Node-Express-API-file-system';
+}
---
+++ backend/api/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import * as config from '../config';
+
+const app = express();
+const port = 3014;
+
+app.get('/', (req, res) => {
+	res.json({
+		appName: config.getAppName() 
+	});
+});
+
+app.listen(port, () => {
+	console.log(`server is running at http://localhost:${port}`);
+});
