8e448034a1b20a46a7ca174e398cfa83fdc94ed7
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-03 11:41:50 +0100

    Add CORS middleware and new API endpoint

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import * as config from '../config';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 const port = 3014;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
+	res.status(203);
 	res.json({
 		appName: config.getAppName() 
 	});
 });
+
+app.get('/node-version', (_, res) => {
+	res.json(999);
+})
 
 app.listen(port, () => {
 	console.log(`server is running at http://localhost:${port}`);
